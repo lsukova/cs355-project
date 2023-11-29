d3a66cea6017af3f77747ae3d3bc16f113586ed9
Chase Thompson <43761034+NebulaVGC@users.noreply.github.com>
2023-11-28 23:42:19 -0500

    Added signatures

--- alice.py
+++ alice.py
@@ -1,19 +1,18 @@
 import socket
 from Crypto.PublicKey import RSA
 import hashlib 
-
+from Crypto.Signature import pkcs1_15
+from Crypto.Hash import SHA256
 
 #Create keys for encrypting, decrypting, and signing
 def generate_keys():
     modulus_length = 2048
 
     key = RSA.generate(modulus_length)
-    #print (key.exportKey())
+    alice_public = key.public_key().export_key("PEM")
+    alice_private = key.export_key("PEM")
 
-    pub_key = key.publickey()
-    #print (pub_key.exportKey())
-
-    return key, pub_key
+    return alice_public, alice_private
 
 #Read from each of the 5 files, hashes them, and combines them into a final message.
 #Add each hash to the dictionary, alice_hashes
@@ -24,14 +23,14 @@ def read_from_file(alice_hashes):
         file = open(file_name, "r")
         content = file.read()
         result = hashlib.sha256(content.encode())
-        final_message = final_message + "-----BEGIN MESSAGE-----\n" + result.hexdigest() + "\n-----END MESSAGE-----"
+        final_message = final_message + "-----BEGIN MESSAGE-----\n" + result.hexdigest() + "\n-----END MESSAGE-----\n"
         alice_hashes[result.hexdigest()] = i
     return final_message
     
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
 
-private, public = generate_keys()
+alice_public, alice_private = generate_keys()
 alice_hashes = {}
 
 #Connect to bob using sockets
@@ -42,22 +41,48 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     with conn:
         while True:
             
+            #This is the public key from bob, which we assume alice receives it without being tampered with
+            bob_public = conn.recv(4096).decode()
+
+            conn.sendall(alice_public)
+            
             #This is the message from bob with the hash values and signatures
-            #TODO: Implement encryption, decryption, and signing
             data = conn.recv(4096)
-            if not data:
-                break
+            
+             #Parse the signature from Alice
+            bob_signature = data.split(b"\n-----END SIGNATURE-----\n")
+            bob_signature = bob_signature[0].split(b"-----BEGIN SIGNATURE-----\n")
+            bob_signature = bob_signature[1].decode()
+            hashes = data.split(b"\n-----END SIGNATURE-----\n")
+            hashes = hashes[1].decode()
+    
+            #Verify the signature and exit if not verified
+            bob_pub_key = RSA.import_key(bob_public)
+            hashed_msg = SHA256.new(bytes(hashes, "utf-8"))
+            try:
+              pkcs1_15.new(bob_pub_key).verify(hashed_msg, bytes(bob_signature, "latin-1"))
+              print("The signature is valid.")
+            except Exception as e:
+              print ("The signature is not valid.")
+              exit(1)
+            
             final_message = read_from_file(alice_hashes)
+            
+            #Sign the message and add it to the final_message
+            priv_key = RSA.import_key(alice_private)
+            hashed_msg = SHA256.new(bytes(final_message, "utf-8"))
+            signature = pkcs1_15.new(priv_key).sign(hashed_msg)       
+            final_message = "-----BEGIN SIGNATURE-----\n" + signature.decode("latin-1") + "\n-----END SIGNATURE-----\n" + final_message
             conn.sendall(bytes(final_message, "utf-8"))
             
             #Split the message into 5 separate parts
-            hashes = data.split(b"-----BEGIN MESSAGE-----\n")
-            hashes = b''.join(hashes)
-            hashes = hashes.split(b"\n-----END MESSAGE-----")
+            hashes = hashes.split("-----BEGIN MESSAGE-----\n")
+            hashes = ''.join(hashes)
+            hashes = hashes.split("\n-----END MESSAGE-----\n")
             
             #Check if any of the hashes are the same
             for hash in hashes:
-                if hash.decode() in alice_hashes:
-                    print("file" + str(alice_hashes[hash.decode()]) + ".txt is the same as one of Bob's files")
+                if hash in alice_hashes:
+                    print("file" + str(alice_hashes[hash]) + ".txt is the same as one of Bob's files")
             exit(0)
             
--- bob.py
+++ bob.py
@@ -1,6 +1,8 @@
 import socket
 from Crypto.PublicKey import RSA
 import hashlib 
+from Crypto.Hash import SHA256
+from Crypto.Signature import pkcs1_15
 
 
 #Create keys for encrypting, decrypting, and signing
@@ -8,12 +10,10 @@ def generate_keys():
     modulus_length = 2048
 
     key = RSA.generate(modulus_length)
-    #print (key.exportKey())
+    bob_public = key.public_key().export_key("PEM")
+    bob_private = key.export_key("PEM")
 
-    pub_key = key.publickey()
-    #print (pub_key.exportKey())
-
-    return key, pub_key
+    return bob_public, bob_private
 
 #Read from each of the 5 files, hashes them, and combines them into a final message.
 #Add each hash to the dictionary, bob_hashes
@@ -24,7 +24,7 @@ def read_from_file(bob_hashes):
         file = open(file_name, "r")
         content = file.read()
         result = hashlib.sha256(content.encode())
-        final_message = final_message + "-----BEGIN MESSAGE-----\n" + result.hexdigest() + "\n-----END MESSAGE-----"
+        final_message = final_message + "-----BEGIN MESSAGE-----\n" + result.hexdigest() + "\n-----END MESSAGE-----\n"
         bob_hashes[result.hexdigest()] = i
     return final_message
 
@@ -32,18 +32,50 @@ HOST = "127.0.0.1"  # The server's hostname or IP address
 PORT = 65432  # The port used by the server
 bob_hashes = {}
 
+bob_public, bob_private = generate_keys()
+
 #Connect to alice using sockets
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
+    s.sendall(bob_public)
+    
     final_message = read_from_file(bob_hashes)
+    
+    #Sign the message and add it to the final_message
+    priv_key = RSA.import_key(bob_private)
+    hashed_msg = SHA256.new(bytes(final_message, "utf-8"))
+    signature = pkcs1_15.new(priv_key).sign(hashed_msg)       
+            
+    final_message = "-----BEGIN SIGNATURE-----\n" + signature.decode("latin-1") + "\n-----END SIGNATURE-----\n" + final_message
+    s.sendall(bytes(final_message, "utf-8"))
+    
+    alice_public = s.recv(4096)
     s.sendall(bytes(final_message, "utf-8"))
     data = s.recv(4096)
+    
+    #Parse the signature from Alice
+    alice_signature = data.split(b"\n-----END SIGNATURE-----\n")
+    alice_signature = alice_signature[0].split(b"-----BEGIN SIGNATURE-----\n")
+    alice_signature = alice_signature[1].decode()
+    hashes = data.split(b"\n-----END SIGNATURE-----\n")
+    hashes = hashes[1].decode()
+    
+    #Verify the signature and exit if not verified
+    alice_pub_key = RSA.import_key(alice_public)
+    hashed_msg = SHA256.new(bytes(hashes, "utf-8"))
+    try:
+        pkcs1_15.new(alice_pub_key).verify(hashed_msg, bytes(alice_signature, "latin-1"))
+        print("The signature is valid.")
+    except Exception as e:
+        print ("The signature is not valid.")
+        exit(1)
+    
     #Split the message into 5 separate parts
-    hashes = data.split(b"-----BEGIN MESSAGE-----\n")
-    hashes = b''.join(hashes)
-    hashes = hashes.split(b"\n-----END MESSAGE-----")
+    hashes = hashes.split("-----BEGIN MESSAGE-----\n")
+    hashes = ''.join(hashes)
+    hashes = hashes.split("\n-----END MESSAGE-----\n")
             
     #Check if any of the hashes are the same
     for hash in hashes:
-        if hash.decode() in bob_hashes:
-            print("file" + str(bob_hashes[hash.decode()]) + ".txt is the same as one of Bob's files")
+        if hash in bob_hashes:
+            print("file" + str(bob_hashes[hash]) + ".txt is the same as one of Alices's files")
